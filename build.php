1b2620d4a90959f0d2ec6e6f380b53aff2aa774e
weichengjun <1054487195@qq.com>
2026-04-07 11:54:57 +0800

    添加PHP导出配置选项并更新构建脚本

--- build.php
+++ build.php
@@ -19,6 +19,30 @@ $protoFiles = shell_exec("find {$protoDir} -type f -name '*.proto'");
 // 将找到的 .proto 文件路径转换为数组
 $protoFilesArray = explode("\n", trim($protoFiles));
 
+// 定义需要添加的选项
+$namespaceOptions = <<<'EOT'
+// --- PHP 导出配置 ---
+option php_namespace = "GrpcSdk\\\\Proto";
+option php_metadata_namespace = "GrpcSdk\\\\GPBMetadata";
+EOT;
+
+// 遍历每个 .proto 文件并添加选项
+foreach ($protoFilesArray as $file) {
+    if (!empty($file)) {
+        // 读取文件内容
+        $content = file_get_contents($file);
+
+        // 检查是否已经包含选项
+        if (strpos($content, 'option php_namespace') === false) {
+            // 在 package 之后插入选项
+            $content = preg_replace('/(package\s+[^;]+;)/', '$1' . "\n" . $namespaceOptions, $content);
+
+            // 写回文件
+            file_put_contents($file, $content);
+        }
+    }
+}
+
 // 构建 protoc 命令
 $protocCommand = "protoc -I {$protoDir} --php_out={$outputDir}";
 foreach ($protoFilesArray as $file) {
@@ -29,6 +53,5 @@ foreach ($protoFilesArray as $file) {
 
 // 执行 protoc 命令
 exec($protocCommand);
-// 执行额外的配置文件格式化
-exec("php {$baseDir}/format_config.php");
+
 echo 'Build Protobuf Success' . PHP_EOL;
